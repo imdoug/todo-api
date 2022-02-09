852c0ca705b14edcd4c9e08dd8ccf7ae10400e60
Doug Moreira <dougmoreira@Dougs-MBP-125.lan>
2022-02-09 17:37:34 -0500

    add item route working

--- controller/item.js
+++ controller/item.js
@@ -13,6 +13,15 @@ router.get('/', (req, res) => {
 
 //INSERT ITEM 
 
+router.post('/', (req, res) => {
+    const {title, description} = req.body
+    postgres.query(`INSERT INTO item (title, description) VALUES ('${title}','${description}')`, (err, results) => {
+        postgres.query('SELECT * FROM item ORDER BY item_id ASC;', (err, results)=>{
+            res.json(results.rows)
+        })
+    }); 
+});
+
 //UPDATE ITEM 
 
 //DELETE ITEM 
